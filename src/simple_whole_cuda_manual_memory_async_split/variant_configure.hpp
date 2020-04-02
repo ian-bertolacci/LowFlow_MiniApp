8d7646ddc1cdaa79b9ab89231f2aff703abe2097
Ian Bertolacci <ian.bertolacci@gmail.com>
2020-04-02 16:11:18 -0700

    adds chunk and streams CLI option

--- src/simple_whole_cuda_manual_memory_async_split/variant_configure.hpp
+++ src/simple_whole_cuda_manual_memory_async_split/variant_configure.hpp
@@ -4,13 +4,19 @@
 #include <global_configure.hpp>
 
 typedef struct struct_VariantOptions {
-
+  int chunks;
+  int streams;
 } VariantOptions;
 
-static const VariantOptions VariantOptions_Default;
+static const VariantOptions VariantOptions_Default {
+  .chunks = 10,
+  .streams = 5
+};
 
 static const struct option variant_long_options[] = {
-  {"help", no_argument, nullptr, 'h'},
+  {"chunks",  required_argument, nullptr, 'c'},
+  {"streams", required_argument, nullptr, 'r'},
+  {"help",    no_argument,       nullptr, 'h'},
   LONG_OPTIONS_TERMINAL
 };
 
